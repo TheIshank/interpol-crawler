cba86a006143093251d6e4cb29f1600317c0f01c
Ishank Saxena <ish.sxn@gmail.com>
2018-07-11 01:51:35 +0530

    Wanted Nation Added

--- criminal-data-extractor.py
+++ criminal-data-extractor.py
@@ -4,47 +4,56 @@ import re
 import csv
 import pandas as pd
 
-# Function to identify which columns are available for the given crimminal profile
+criminal_ids = pd.read_csv("criminals.csv")
 
+wanted_url = "https://www.interpol.int/notice/search/wanted/"
+
+columns = ["criminal_id","present_family_name","forename","sex","dob","place_of_birth","language","nationality","height","weight","hair","eyes","charges"]
+details = ["Criminal ID:","Present family name:","Forename:","Sex:","Date of birth:","Place of birth:","Language spoken:","Nationality:","Height:","Weight:","Colour of hair:","Colour of eyes:","Charges:"]
+
+df = pd.DataFrame(columns=columns)
 
 def getAvailCols(availables):
     avail_cols = []
-    avail_cols.append("criminal_id")
-    details = ["Criminal ID:", "Present family name:", "Forename:", "Sex:", "Date of birth:", "Place of birth:",
-               "Language spoken:", "Nationality:", "Height:", "Weight:", "Colour of hair:", "Colour of eyes:", "Charges:"]
+    details = ["Criminal ID:","Present family name:","Forename:","Sex:","Date of birth:","Place of birth:","Language spoken:","Nationality:","Height:","Weight:","Colour of hair:","Colour of eyes:","Charges:"]
+#     print("\nAvailables: ",availables)
     for available in availables:
+#         print("\nAvailable: ",available)
         if(available.text == '\xa0'):
             continue
         elif(available.text in details):
             avail_cols.append(columns[details.index(available.text)])
+#         print(avail_cols)
     return avail_cols
 
-
-criminal_ids = pd.read_csv("criminals.csv")
-wanted_url = "https://www.interpol.int/notice/search/wanted/"
-columns = ["criminal_id", "present_family_name", "forename", "sex", "dob", "place_of_birth",
-           "language", "nationality", "height", "weight", "hair", "eyes", "charges"]
-details = ["Criminal ID:", "Present family name:", "Forename:", "Sex:", "Date of birth:", "Place of birth:",
-           "Language spoken:", "Nationality:", "Height:", "Weight:", "Colour of hair:", "Colour of eyes:", "Charges:"]
-df = pd.DataFrame(columns=columns)
-
-for criminal_id in criminal_ids.criminal_id[1112:]:
-    print(criminal_id)
+for criminal_id in criminal_ids.criminal_id:
+#     print(criminal_id)
     url = wanted_url+str(criminal_id)
     response = requests.get(url)
-    soup = bs(response.content, 'lxml')
+    soup = bs(response.content,'lxml')
     availables = soup.findAll("td", {"class": "col1"})
     values = soup.findAll("td", {"class": "col2"})
-    avail_cols = getAvailCols(availables)
+#     if(not getAvailCols(availables)):
+#         continue
+    avail_cols = ["criminal_id","wanted_by"]
+    avail_cols+=getAvailCols(availables)
     values_text = []
     values_text.append(criminal_id)
-    for i, available in enumerate(availables):
+    values_text.append(soup.findAll("span", {"class": "nom_fugitif_wanted_small"})[0].text.split("authorities of")[-1].strip())
+    for i,available in enumerate(availables):
         if(availables[i].text in details):
-            value = values[i].text.replace('\t', '').replace('\n', '')
+            value = values[i].text.replace('\t','').replace('\n','')
             values_text.append(value)
     values = []
     values.append(values_text)
     df_criminal = pd.DataFrame(values, columns=avail_cols)
-    df = pd.concat([df, df_criminal], axis=0, ignore_index=True)
+    df = pd.concat([df,df_criminal], axis=0, ignore_index=True)
+#     print(availables)
+#     print(values)
+dobs = []
+for dob in df.dob:
+    dob = str(dob).split(" ")[0]
+    dobs.append(dob)
+df.dob = dobs
 
 df.to_csv("criminal_data.csv")
